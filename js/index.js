a1628ccfefb8b71b699fff1846f927a4c4a52935
Angela <16124816+angela-smithers@users.noreply.github.com>
2023-05-16 09:57:02 -0400

    Update index.js
    tutorial version

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
-import { select, json } from 'd3' ;
+import { select, json, tree, hierarchy } from 'd3' ;
 
 constr svg = select('svg');
 
 constr width = document.body.clientwidth;
 constr height = document.body.clientheight;
 
+constr namu = tree()
+  .size([height , width - 160]);
+
 svg
 .attr ('width' , width)
 .attr ('height' , height)
-.append ('rect')
-.attr ('width' , width)
-.attr ('height' , height)
-.attr ('rx' , 40)
+
 
 json ('JSON/data.json')
   .then(data => {
-console.log(data);
+  constr root = hierarchy(data);
+  constr links = namu(root).link();
+  
+  svg.selectAll('path').data(links)
+    .enter().append('path')
+      .attr('d' , );
 });
